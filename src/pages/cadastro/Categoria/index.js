1fdd5b2265716d6496b773edde576a5778e4ed79
Tayane Fernandes <tayane.rmf@gmail.com>
2020-07-30 14:43:20 -0300

    Style form

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,16 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
-import {Link} from 'react-router-dom'
+import FormField from '../../../components/FormField';
+import Button from '../../../components/Button';
 
 const CadastroCategoria = () => {
-    return (
-       <PageDefault>
-           <h1>Cadastro de Categoria</h1>
-           <Link to="/">
-               Home
-           </Link>
-       </PageDefault>
-    )
-}
+  const valoresIniciais = {
+    nome: '',
+    descricao: '',
+    cor: '',
+  };
+
+  const [values, setValues] = useState(valoresIniciais);
+  const [categorias, setCategorias] = useState([]);
+
+  const setValue = (chave, valor) => {
+    setValues({
+      ...values,
+      [chave]: valor,
+    });
+  };
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setValue(e.target.getAttribute('name'), value);
+  };
+
+  return (
+    <PageDefault>
+      <h1>
+        Cadastro de Categoria:
+        {values.nome}
+      </h1>
+
+      <form onSubmit={(e) => {
+        e.preventDefault();
+
+        setCategorias([
+          ...categorias,
+          values,
+        ]);
+
+        setValues(valoresIniciais);
+      }}
+      >
+        <FormField value={values.nome} onChange={handleChange} name="nome" label="Nome" type="text" />
+        <FormField value={values.descricao} onChange={handleChange} name="descricao" label="Descrição" type="textarea" />
+        <FormField value={values.cor} onChange={handleChange} name="cor" label="Cor" type="color" />
+        <Button>
+          Cadastrar
+        </Button>
+      </form>
+
+      <ul>
+        {categorias.map((categoria, index) => (
+          <li key={`${categoria.nome}${index}`}>
+            {categoria.nome}
+            <br />
+            {categoria.descricao}
+            <br />
+            {categoria.cor}
+            <br />
+          </li>
+        ))}
+      </ul>
+
+      <Link to="/">
+        Ir para home
+      </Link>
+    </PageDefault>
+  );
+};
 
 export default CadastroCategoria;
--- src/pages/home/App.js
+++ src/pages/home/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import Menu from '../../components/Menu'
 import dadosIniciais from '../../data/dados_iniciais.json'
 import BannerMain from '../../components/BannerMain'
 import Carousel from '../../components/Carousel'
-import Footer from '../../components/Footer'
 import PageDefault from '../../components/PageDefault';
 
 function Home() {
